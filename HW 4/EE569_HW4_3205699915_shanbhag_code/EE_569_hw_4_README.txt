f406eaf5dcef9b928bdd7e0da3e99d3d764cd8f3
Aakash Shanbhag <adshanbh@usc.edu>
2017-06-05 16:41:58 -0700

    Add files via upload

---
+++ HW 4/EE569_HW4_3205699915_shanbhag_code/EE_569_hw_4_README.txt
@@ -0,0 +1,11 @@
+EE 569 README
+AAKASH SHANBHAG
+HW #4
+USC id:3205699915
+Email: adshanbh@usc.edu
+
+To open the the .py files in both the folders, You need to install the software package of tflearn from the link : http://tflearn.org/installation/ and select the package according to system configuration.
+
+Once downloaded, on the terminal you can compile the .py files and run the code successfully.
+
+For the output viewer: TENSORBOARD can be utilised by: tensorboard --logdir=path/to/log-directory
---
+++ HW 4/EE569_HW4_3205699915_shanbhag_code/Prob2/prob2.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+# Partial Author : AAKASH SHANBHAG.
+#Problem: 2
+# Code credited to: tflearn/examples/CIFAR_10
+#Github link:https://github.com/tflearn/tflearn/blob/master/examples/images/convnet_cifar10.py
+
+""" Convolutional network applied to CIFAR-10 dataset classification task.
+
+References:
+    Learning Multiple Layers of Features from Tiny Images, A. Krizhevsky, 2009.
+
+Links:
+    [CIFAR-10 Dataset](https://www.cs.toronto.edu/~kriz/cifar.html)
+
+"""
+from __future__ import division, print_function, absolute_import
+
+import tflearn
+import tensorflow as tf
+from tflearn.data_utils import shuffle, to_categorical
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.estimator import regression
+from tflearn.data_preprocessing import ImagePreprocessing
+from tflearn.data_augmentation import ImageAugmentation
+
+
+# Data loading and preprocessing
+from tflearn.datasets import cifar10
+(X, Y), (X_test, Y_test) = cifar10.load_data()
+X, Y = shuffle(X, Y)
+Y = to_categorical(Y, 10)
+Y_test = to_categorical(Y_test, 10)
+
+# Real-time data preprocessing
+img_prep = ImagePreprocessing()
+img_prep.add_featurewise_zero_center()
+img_prep.add_featurewise_stdnorm()
+
+# Real-time data augmentation
+img_aug = ImageAugmentation()
+img_aug.add_random_flip_leftright()
+img_aug.add_random_rotation(max_angle=25.)
+
+# Convolutional network building
+network = input_data(shape=[None, 32, 32, 3],
+                     data_preprocessing=img_prep,
+                     data_augmentation=img_aug)
+network = conv_2d(network, 24, 5,padding='valid',activation='leaky_relu',weights_init='Xavier')
+network = max_pool_2d(network, 2,padding='valid')
+network = conv_2d(network, 64, 5,padding='valid',activation='leaky_relu',weights_init='Xavier')
+network = max_pool_2d(network, 2,padding='valid')
+network = fully_connected(network, 240, activation='leaky_relu')
+network = dropout(network, 0.9)
+network = fully_connected(network, 160, activation='leaky_relu')
+network = dropout(network, 0.9)
+
+network = fully_connected(network, 10, activation='softmax')
+network = regression(network, optimizer='adam',
+                     loss='categorical_crossentropy',
+                     learning_rate=0.001)
+
+# Train using classifier
+model = tflearn.DNN(network, tensorboard_verbose=0)
+model.fit(X, Y, n_epoch=100, shuffle=True, validation_set=(X_test, Y_test),
+          show_metric=True, batch_size=256, run_id='cifar10_cnn')
